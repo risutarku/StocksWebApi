e0925ac32bc85ca3d6fb9d8327799a97a39e7edc
Балашов Антон <vity.esenin@mail.ru>
2024-03-31 23:36:01 +0700

    add pagination to getAll Stocks

--- asp.net web api test/Repostirory/StockRepository.cs
+++ asp.net web api test/Repostirory/StockRepository.cs
@@ -60,7 +60,9 @@ namespace asp.net_web_api_test.Repostirory
                 }
             }
 
-            return await stocks.ToListAsync();
+            var skipNumber = (query.PageNumber - 1) * query.PageSize;
+
+            return await stocks.Skip(skipNumber).Take(query.PageSize).ToListAsync();
 
         }
          
